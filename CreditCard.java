d082f881348a18e082704c63ae7626718994ea3e
anthony escalante <anthony.escalante@revature.net>
2021-08-25 14:09:27 -0400

    finished CreditCard

--- CreditCard.java
+++ CreditCard.java
@@ -4,23 +4,35 @@ public class CreditCard{
 	public static void main(String[] args){
 		Scanner stdin = new Scanner(System.in);
 		CreditCard c = new CreditCard();
-//		System.out.println("Enter card number");
-//		int n = stdin.nextInt();
-		int n = 128;
+		System.out.println("Enter card number");
+		long n = stdin.nextLong();
 		System.out.println(c.validateCard(n));
-		System.out.println(c.validateCard(1234567890123456));
-		System.out.println(c.validateCard(1234567890123452));
+		System.out.println(c.validateCard(1234567890123456l));
+		System.out.println(c.validateCard(1234567890123452l));
 	}
-	public boolean validateCard(int card){
-		boolean res = false;
-		int check = card%10;
-		int n = card/10;
-		StringBuilder s = new StringBuilder(Integer.toString(n));
+	public boolean validateCard(long card){
+		int check = (int)card%10;
+		int count=0;
+		long n = card/10;
+		StringBuilder s = new StringBuilder(Long.toString(n));
 		s = s.reverse();
 		int arr[]= new int[s.length()];
 		for (int i=0;i< arr.length; i++){
-			
+			if (i%2==0){
+				arr[i]= 2*(s.charAt(i)-'0');
+				if (arr[i]/10>0){
+					arr[i]= (arr[i]/10)+(arr[i]%10);
+				}
+				count+=arr[i];
+			}else{
+				arr[i]= (s.charAt(i)-'0');
+				count+=arr[i];
+			}
 		}
-		return res;
+		if(10-(count%10)==check){
+			return true;
+		}else {
+			return false;
+		}
 	}
 }
